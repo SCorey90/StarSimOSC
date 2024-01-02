d879afc32e79c810058391f74671e0614c7debee
Sam Corey <samcorey@Sams-MBP.attlocal.net>
2024-01-02 15:07:32 -0500

    switched to setting working dir in StartShell.sh

--- StartShell.sh
+++ StartShell.sh
@@ -4,6 +4,8 @@ JOB_DIR=$1
 N_EVT=$2
 JOB_NUM=$3
 
-/users/PAS2524/corey90/star_root5-gcc485.sif <<EOF
-/users/PAS2524/corey90/star_mount/run_starsim+reco.sh $JOB_DIR $N_EVT $JOB_NUM
+export WORKING_DIR=/users/PAS2524/corey90
+
+$WORKING_DIR/star_root5-gcc485.sif <<EOF
+$WORKING_DIR/star_mount/run_starsim+reco.sh $JOB_DIR $N_EVT $JOB_NUM
 EOF
